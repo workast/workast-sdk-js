efab5cabcd75a7c89c92697a6d1b029121b162ee
Matias Fito <matias@workast.com>
2019-12-17 19:15:16 -0300

    Add CleanWebpackPlugin to clean build folders

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const commonConfig = {
   entry: path.resolve(__dirname, 'src/workast.js'),
@@ -34,6 +35,12 @@ const umdConfig = {
     libraryTarget: 'umd',
     globalObject: 'this'
   },
+  plugins: [
+    // Needs to be instantiated per config, sharing the reference to CleanWebpackPlugin
+    // in commonConfig, will result in both build cleaning the path.output
+    // of the first build that was executed.
+    new CleanWebpackPlugin()
+  ],
   devtool: false,
   externals: [
     nodeExternals()
@@ -50,6 +57,9 @@ const browserConfig = {
     libraryTarget: 'window',
     globalObject: 'window'
   },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
   devtool: 'source-map'
 };
 
