c29dc2d72a0f141848bf8819fa9b1f3968c13bc8
Fernando Buccella <fer.buccella@gmail.com>
2019-12-17 22:18:09 -0300

    Add test case to check a request can be made by impersonating a team.

--- test/workast.spec.js
+++ test/workast.spec.js
@@ -330,5 +330,26 @@ describe('Workast', () => {
 
       stub.restore();
     });
+
+    it('Should make a request impersonating a team', async () => {
+      const method = 'GET';
+      const path = '/user';
+      const team = chance.hash();
+      const responseBody = [{ id: chance.md5(), name: chance.name() }];
+
+      const scope = nock(workast.config.apiBaseUrl)
+        .get(path)
+        .matchHeader('Accept', 'application/json')
+        .matchHeader('Authorization', `Bearer ${workast.config.token}`)
+        .matchHeader('Content-Type', 'application/json')
+        .matchHeader(Workast.IMPERSONATE_TEAM_HEADER, team)
+        .reply(200, responseBody);
+
+      const users = await workast.apiCall({ method, path, impersonate: { team } });
+      expect(users).to.deep.equal(responseBody);
+      expect(scope.isDone()).to.be.true;
+    });
+
+    it('Should make a request impersonating a user');
   });
 });
