74161d0c48c9bcedeffa1476a56dc3b946ae6b7c
Matias Fito <matias@workast.com>
2020-08-22 08:31:30 -0300

    Add tags resource

--- src/resources/index.js
+++ src/resources/index.js
@@ -2,10 +2,12 @@
 
 const lists = require('./lists');
 const notifications = require('./notifications');
+const tags = require('./tags');
 const tasks = require('./tasks');
 
 module.exports = {
   lists,
   notifications,
+  tags,
   tasks
 };
